473d7b311add084504e397479d54368e7d136db9
Nima <nima97e@gmail.com>
2022-10-25 18:03:49 +0330

    crete GlobalState

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import Header from './components/Header'
 import Balance from './components/Balance'
+import IncomeExpenses from './components/IncomeExpenses'
+import TransactionList from './components/TransactionList'
+import AddTransaction from './components/AddTransaction'
 import './App.css'
 
 
@@ -9,6 +12,9 @@ const App = () => {
         <Header />
         <div className="container">
             <Balance />
+            <IncomeExpenses />
+            <TransactionList />
+            <AddTransaction />
         </div>
     </div>
   )
