52bedc4a1b221aa5e96f70a148d137561a58d786
Nima <nima97e@gmail.com>
2022-10-25 23:30:31 +0330

    add transaction

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -4,12 +4,7 @@ import AppReducer from './AppReducer'
 // initial state
 
 const initialState = {
-    transactions: [
-        { id: 1, text: 'Flower', amount: -20 },
-        { id: 2, text: 'Salary', amount: 300 },
-        { id: 3, text: 'Book', amount: -10 },
-        { id: 4, text: 'Camera', amount: 150 }
-    ]
+    transactions: []
 }
 
 // Create context
@@ -22,11 +17,24 @@ export const GlobalProvider = ({ children }) => {
 
     // Actions
     function deleteTransaction(id){
-        
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+
+        })
+    }
+    function addTransaction(transaction){
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction,
+
+        })
     }
 
     return (<GlobalContext.Provider value={{
-        transactions: state.transactions
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
     }}> 
         {children}
     </GlobalContext.Provider>)
